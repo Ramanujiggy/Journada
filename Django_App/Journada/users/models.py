f35d359c2ff69b315b7e5b00a0c3094f433bcd3a
Robert <Robertdgelin@hotmail.com>
2023-12-09 14:32:17 -0600

    added user and Session models

--- Django_App/Journada/users/models.py
+++ Django_App/Journada/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+#creating user model for database
+class User(models.Model):
+    user_id=  models.BigAutoField(primary_key=True)
+    username= models.CharField(max_length=30, blank=False)
+    password= models.CharField(max_length=30, blank=False)
+    user_email= models.CharField(max_length=30,blank=False)
+
+#creating session model for  database
+class Session(models.Model):
+    session_id=models.BigAutoField(primary_key=True)
+    date=models.DateField(default=datetime.date.today())
+    time=models.TimeField(default=datetime.time.now())
+    start_time=models.TimeField(default=datetime.datetime.now()) #the start of the 'duration'
+
+
+
+
+
