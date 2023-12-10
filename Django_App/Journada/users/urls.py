78bc290180c8af69c1498e7e1e4a2c1a7de52481
Robert <Robertdgelin@hotmail.com>
2023-12-10 10:59:00 -0600

    updating views

--- Django_App/Journada/users/urls.py
+++ Django_App/Journada/users/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
-    #path('dashboard/', views.contact, name='dashboard') not live yet
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('log_session/',views.log_sesssion,name='Log Session')
    ]
--- Django_App/Journada/users/views.py
+++ Django_App/Journada/users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from .models import User
+
+#views MUST contain http response 
 
 
 
@@ -11,8 +14,10 @@ def register(request):
      return HttpResponse("Hello welcome to Journada!")
 
 def log_sesssion(request, session_id):
-          response= "Log a training session here! Session id is %"
+          response= "Log a training session here! Session id is % --NOT BUILT--"
           return HttpResponse(response % session_id)
 
-def log_view(request, user_id):
-          return
+def dashboard(request, user_id): #returns index of all training sessions and the user 
+     all_sessions=User.object.session_id.order_by(date)
+     output=", ".join("training session on:" for s in session_id) 
+     return HttpResponse(all_sessions)
