94d0004670c7b276824fd68fa9ae13f509155838
Robert <Robertdgelin@hotmail.com>
2023-12-11 23:08:06 -0600

    still working on url pathing

--- Django_App/Journada/mysite/urls.py
+++ Django_App/Journada/mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
