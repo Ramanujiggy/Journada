d9876bab1877f9f85f84034200967cd58a83268f
ramanujiggy <Robertdgelin@gmail.com>
2024-01-04 14:38:51 -0800

    making changes to codebase to move development to cloud hosting.

---
+++ Django_App/Journada/mysite/deployment.py
@@ -0,0 +1,41 @@
+import os 
+from .settings import *
+from .settings import BASE_DIR
+
+#this will be a hostname provided by azure 
+ALLOWED_HOSTS = [os.enviorn['WEBSITE_HOSTNAME']] 
+CSRF_TRUSTED_ORIGINS = ['https://' + os.enviorn['WEBSITE_HOSTNAME']]
+DEBUG = False 
+
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+
+
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+paramenters = {pair.split('='):{pair.split('-')[1]for pair in connection_string.split(' ')}} 
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parameters['dbname'],
+        'USER': parameters['host'],
+        'PASSWORD': parameters['user'],
+        'HOST': parameters['password'],
+    }
+}
--- Django_App/Journada/mysite/wsgi.py
+++ Django_App/Journada/mysite/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.enviorn else 'mysite.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
