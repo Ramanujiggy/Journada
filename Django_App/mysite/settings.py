9c79f0eb75a9694511384756a4e128309b2720c2
Rob <robertdgelin@hotmail.com>
2024-01-10 08:34:47 -0800

    Update settings.py

--- Django_App/mysite/settings.py
+++ Django_App/mysite/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Django_App.accounts',
-    'Django_App.users',
+    'accounts',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
