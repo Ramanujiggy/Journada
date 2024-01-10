4ae0e23339c30126e4172ae6bc89fe3303878492
Rob <robertdgelin@hotmail.com>
2024-01-09 17:19:24 -0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'Django_App.mysite.deployment' if 'WEBSITE_HOSTNAME' in os.enviorn else 'mysite.settings'
+    settings_module = 'Django_App.mysite.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'mysite.settings'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
