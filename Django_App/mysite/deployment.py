46f093bfee6b5c1cd238530c61aa25607f325958
Rob <robertdgelin@hotmail.com>
2024-01-09 22:27:49 -0800

    Update deployment.py
    Changing connection string to tuple instead of list to avoid type error 'unable to hash list'

--- Django_App/mysite/deployment.py
+++ Django_App/mysite/deployment.py
@@ -27,7 +27,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+connection_string = os.environ('AZURE_POSTGRESQL_CONNECTIONSTRING')
 parameters = {pair.split('='):pair.split('=')[1]for pair in connection_string.split(' ')} 
 
 
