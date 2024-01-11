3a1244a63d1923a0c3a0a4bd937a9a0026e22f0c
ramanujiggy <Robertdgelin@gmail.com>
2024-01-10 20:46:30 -0800

    adding 'CUSTOMCONNSTR_' prefix to Azure connection string

--- mysite/deployment.py
+++ mysite/deployment.py
@@ -30,7 +30,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 #connection_string = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING')
 #parameters = {pair.split('=')[0]:pair.split('=')[1]for pair in connection_string.split(' ')} 
 
-connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+connection_string = os.environ['CUSTOMCONNSTR_AZURE_POSTGRESQL_CONNECTIONSTRING']
 parameters = {pair.split('='):pair.split('=')[1]for pair in connection_string.split(' ')} 
 
 DATABASES = {
