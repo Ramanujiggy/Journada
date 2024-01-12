4c330b9f2a4e1ab34c4b6437d9b278ea24a553af
ramanujiggy <Robertdgelin@gmail.com>
2024-01-12 14:58:52 -0800

    had syntax error

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from accounts import views
 
 
 urlpatterns = [
-    path("/signup/", views.register, name="signup"),
+    path("signup/", views.register, name="signup"),
     # path('account/login',views.login,name='login')
 ]
