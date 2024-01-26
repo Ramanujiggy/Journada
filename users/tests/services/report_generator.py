50e82d7725ac975301be38bed86aab3f8e0c3354
jasonb <jw_brandt@outlook.com>
2024-01-26 12:15:38 -0600

    Stub out the unit test

--- users/tests/services/report_generator.py
+++ users/tests/services/report_generator.py
@@ -5,8 +5,9 @@ from users.services import report_generator
 
 class ReportGeneratorTest(unittest.TestCase):
     def test_generate_report(self):
+        # Add unit test here for the report generator
+        pass
         report = report_generator.generate_report(1)
-        self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
