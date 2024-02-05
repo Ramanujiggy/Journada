f7ec6ef68390bc310f18ac1ad90af891341e78b1
ramanujiggy <robertdgelin@gmail.com>
2024-02-05 14:17:49 -0600

    fixing syntax error on urls.py

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("log_session", views.log_session, name="log_session"),
-    path("journal_entries", views.list_journal_notes, name="list_journal_notes")
+    path("journal_entries", views.list_journal_notes, name="list_journal_notes"),
     path(
         "edit_grapple_entry/<int:grapple_entry_id>",
         views.edit_grapple_entry,
