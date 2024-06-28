4280c091b5a59766f78455b951d20b9e310e0fd4
ramanujiggy <robertdgelin@gmail.com>
2024-06-27 19:59:39 -0500

    setting up dashboard values, working on adding total gi hours

--- users/services/grapple_entry_service.py
+++ users/services/grapple_entry_service.py
@@ -21,12 +21,19 @@ def generate_report(user_id: int) -> Report:
         grapple_entries.aggregate(total_minutes=Sum("minutes_trained"))["total_minutes"]
         or 0
     )
+    
+    total_gi_entries=grapple_entries.filter(grappling_type='Gi')
+    
+    total_gi_hours = total_gi_entries.aggregate(gh=Sum("hours_trained"))["gh"] or 0 
+
 
     total_mat_time = round(total_hours_trained + (total_minutes_trained // 60))
     # current_session_id = GrappleEntry.objects.get(id)
     return Report(
+        
         grapple_entries=grapple_entries,
         hours_trained=total_hours_trained,
         minutes_trained=total_minutes_trained,
         total_mat_time=total_mat_time,
-    )
+        all_gi_hours=total_gi_hours
+        )
--- users/views.py
+++ users/views.py
@@ -61,7 +61,6 @@ def dashboard(request):
     """returns all training sessions for specific user"""
     user = get_user(request)
     user_id = user.id
-    currentUser= get_user(user)
 
     report = grapple_entry_service.generate_report(user_id=user_id)
 
@@ -73,7 +72,7 @@ def dashboard(request):
             "hours_trained": report.hours_trained,
             "minutes_trained": report.minutes_trained,
             "total_mat_time": report.total_mat_time,
-            "gi_hours":report.grappling_type_gi,
+            "gi_hours":report.all_gi_hours,
             "nogi_hours":report.grappling_type_nogi
             
         },
